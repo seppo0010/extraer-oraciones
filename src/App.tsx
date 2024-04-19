77af90a6ea4f1d7f2aed32fce1b5abedca68b151
Sebastian Waisbrot <seppo0010@gmail.com>
2024-04-18 21:24:39 -0300

    Add num sentences count

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
   const [palabras, setPalabras] = useState("fuerza\nenergía\nintencionar");
   const [files, setFiles] = useState<File[]>([]);
   const [buscar, setBuscar] = useState(false);
+  const [numSentences, setNumSentences] = useState<number | null>(null);
   const resultados = useAsync(async () => {
     if (!buscar) return;
     return (
@@ -29,43 +30,45 @@ function App() {
               new Uint8Array(await f.arrayBuffer())
             ).promise;
 
-            return (
-              await Promise.all(
-                range(pdf.numPages).flatMap(
-                  async (pageNumber): Promise<Result[]> => {
-                    const page = await pdf.getPage(pageNumber + 1);
-                    const texts = await page.getTextContent();
-                    const pageText = texts.items
-                      .map((item) => (item as { str?: string }).str ?? "")
-                      .join(" ");
-                    return palabras
-                      .toLocaleLowerCase()
-                      .split("\n")
-                      .flatMap((palabra): Result[] => {
-                        const p = palabra.trim();
-                        if (p === "") return [];
-                        const blocks = pageText.split(new RegExp(p, "ig"));
-                        return zip(blocks, blocks.slice(1)).map(
-                          ([before, after]): Result => {
-                            const b = `.${before}`;
-                            const a = `${after}.`;
-                            return {
-                              page: pageNumber + 1,
-                              sentence:
-                                b.substring(b.lastIndexOf(".") + 1) +
-                                "" +
-                                palabra +
-                                "" +
-                                a.substring(0, a.indexOf(".") + 1),
-                              term: palabra,
-                            };
-                          }
-                        );
-                      });
-                  }
-                )
+            let sentences = 0;
+            const res = await Promise.all(
+              range(pdf.numPages).flatMap(
+                async (pageNumber): Promise<Result[]> => {
+                  const page = await pdf.getPage(pageNumber + 1);
+                  const texts = await page.getTextContent();
+                  const pageText = texts.items
+                    .map((item) => (item as { str?: string }).str ?? "")
+                    .join(" ");
+                  sentences += (pageText.match(/\w[.?!](\s|$)/g) ?? []).length
+                  return palabras
+                    .toLocaleLowerCase()
+                    .split("\n")
+                    .flatMap((palabra): Result[] => {
+                      const p = palabra.trim();
+                      if (p === "") return [];
+                      const blocks = pageText.split(new RegExp(p, "ig"));
+                      return zip(blocks, blocks.slice(1)).map(
+                        ([before, after]): Result => {
+                          const b = `.${before}`;
+                          const a = `${after}.`;
+                          return {
+                            page: pageNumber + 1,
+                            sentence:
+                              b.substring(b.lastIndexOf(".") + 1) +
+                              "" +
+                              palabra +
+                              "" +
+                              a.substring(0, a.indexOf(".") + 1),
+                            term: palabra,
+                          };
+                        }
+                      );
+                    });
+                }
               )
-            ).flat();
+            );
+            setNumSentences(sentences);
+            return res.flat();
           }
         )
       )
@@ -108,7 +111,8 @@ function App() {
           {!resultados.loading && !resultados.error && (
             <>
               {!resultados.value && <p>No se encontraron resultados.</p>}
-              {resultados.value && (
+              {resultados.value && (<>
+                {numSentences && <p>Cantidad de oraciones: {numSentences}</p>}
                 <ul>
                   {resultados.value.map((v, i) => (
                     <li key={`${i}`}>
@@ -120,7 +124,7 @@ function App() {
                     </li>
                   ))}
                 </ul>
-              )}
+              </>)}
             </>
           )}
           <button onClick={() => setBuscar(false)} style={{ padding: 6 }}>
